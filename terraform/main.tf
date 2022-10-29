11afde9d9953e2aee9ae05facca225f91630417e
Ieso Dias <iesodias@gmail.com>
2022-10-29 13:16:14 -0300

    automated commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,8 +17,8 @@ resource "azurerm_resource_group" "my_demo_rg1" {
   location = "eastus"
   name = "my-demo-rg1"
 
-  # tags = {
-  #   "environment" = "k8sdev"
-  # }
+  tags = {
+    "environment" = "k8sdev"
+  }
 
 }
