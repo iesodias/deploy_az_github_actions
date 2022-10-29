128ee4afe1ddaa27b9117d3c2b7257cbd6866d5a
Ieso Dias <iesodias@gmail.com>
2022-10-29 13:19:12 -0300

    automated commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,10 +15,10 @@ provider "azurerm" {
 # Create Resource Group 
 resource "azurerm_resource_group" "my_demo_rg1" {
   location = "eastus"
-  name = "my-demo-rg1"
+  name     = "my-demo-rg1"
 
   tags = {
-    "environment" = "k8sdev"
+    "environment"  = "k8sdev"
     "environment1" = "k8sde2v"
   }
 
